9a82a1d0d4683113d9b2e2a9fd8f56eec520fca0
jiasong <593908937@qq.com>
2020-12-23 18:46:04 +0800

    fix

---
+++ JSMediaBrowser/MediaBrowserAppearance.swift
@@ -0,0 +1,16 @@
+//
+//  MediaBrowserAppearance.swift
+//  JSMediaBrowser
+//
+//  Created by jiasong on 2020/12/23.
+//
+
+import UIKit
+
+@objc open class MediaBrowserAppearance: NSObject {
+    
+    static let appearance = MediaBrowserAppearance()
+    
+   
+    
+}
