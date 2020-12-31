6e916f92a422f9ce4af90071697df2bbfc12a400
jiasong <593908937@qq.com>
2020-12-31 16:25:38 +0800

    F

---
+++ Example/JSMediaBrowserExample/General/KingfisherWebImageMediator.swift
@@ -0,0 +1,23 @@
+//
+//  KingfisherWebImageMediator.swift
+//  JSMediaBrowserExample
+//
+//  Created by jiasong on 2020/12/31.
+//  Copyright © 2020 jiasong. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+import JSMediaBrowser
+
+class KingfisherWebImageMediator: NSObject, WebImageMediatorProtocol {
+    
+    func setImage(forView view: UIView?, url: URL?, thumbImage: UIImage?, setImageBlock: WebImageMediatorSetImageBlock?, progress: WebImageMediatorDownloadProgress?, completed: WebImageMediatorCompleted?) {
+        
+    }
+    
+    func cancelImageRequest(forView view: UIView?) {
+        
+    }
+    
+}
